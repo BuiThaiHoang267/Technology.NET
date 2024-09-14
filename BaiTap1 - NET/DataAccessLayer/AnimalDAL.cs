e1a23372bd4007618bdf6937ac3463f7f9fbc167
Bui Thai Hoang <154488027+BuiThaiHoang267@users.noreply.github.com>
2024-09-14 09:04:18 +0700

    Add project files.

---
+++ BaiTap1 - NET/DataAccessLayer/AnimalDAL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap1___NET.DataAccessLayer
+{
+    public class AnimalDAL
+    {
+        private string connectionString = "Data Source=MSI;Initial Catalog=QUANLYTRANGTRAI;Integrated Security=True";
+
+        AnimalDAL() 
+        {
+        }
+    }
+}
---
+++ BaiTap1 - NET/DataAccessLayer/DataClass/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap1___NET.DataAccessLayer.DataClass
+{
+    public abstract class Animal
+    {
+        public int AnimalID;
+        public string AnimalType;
+        public int AnimalAmount;
+        public string Sound;
+        public Animal()
+        {
+            AnimalID = 0;
+            AnimalType = string.Empty;
+            AnimalAmount = 0;
+            Sound = string.Empty;
+        }
+    }
+}
